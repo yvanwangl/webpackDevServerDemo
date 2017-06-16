821712ec134943aee372ffb0482922d4f2478cb0
wangyafei <wangyafei@startimes.com.cn>
2017-06-16 19:00:06 +0800

    init project

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+
+const debug = process.env.NODE_ENV!='production';
+
+const commonPlugins = [
+    new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'app', 'index.html'),
+        alwaysWriteToDisk: true
+    }),
+    new HTMLWebpackHarddiskPlugin({
+        outputPath: path.resolve(__dirname, '/dist')
+    })
+];
+
+module.exports = {
+    entry: path.join(__dirname, 'app', 'index.js'),
+    output: {
+        path: path.join(__dirname,'./dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+    devServer: {
+        inline: true,
+        hot: true,
+        open: true,
+        contentBase: '/dist',
+        historyApiFallback: true
+    },
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                include: path.join(__dirname, 'app'),
+                loader: ['babel-loader']
+            }
+        ]
+    },
+    plugins: debug?[
+        ...commonPlugins,
+        new webpack.HotModuleReplacementPlugin()
+    ]:[
+        ...commonPlugins
+    ]
+};
